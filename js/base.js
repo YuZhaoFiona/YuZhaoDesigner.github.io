3e30211aaed771102ccc6f8982bd06337b29b9db
Yuschoolproject <Fiiona@Yus-MacBook-Pro.local>
2023-11-25 13:41:06 +1300

    Header animation complete

--- js/base.js
+++ js/base.js
@@ -1,3 +1,6 @@
+/**
+ * @returns Document height
+ */
 function documentBottom() {
     return Math.max(
         document.body.scrollHeight,
@@ -8,55 +11,69 @@ function documentBottom() {
     );
 }
 
+/**
+ * @returns Heigth of the document outside of the screen. In other words, the distance between the bottom of the screen,
+ * and the bottom of the document, according to the current scrolling position
+ */
 function distanceToBottom() {
     return Math.floor(documentBottom() - window.scrollY - window.innerHeight)
 }
 
-function getScrollingPosition() {
-    return window.scrollY;
-}
-
 
 document.addEventListener('DOMContentLoaded', function() {
-    parallaxAnimation2(0.3, 'header', getScrollingPosition);
-    parallaxAnimation(-0.5, 'footer', distanceToBottom);
+    headerAnimation(0.5, 'header');
+    footerAnimation(-0.5, 'footer', distanceToBottom);
 });
 
-function parallaxAnimation(rate, selector, func) {
+/**
+ * Determine the scrolliing change in comparison with a previous state.
+ * Negative when scrolling up, positive when scrolling down
+ */
+
+let oldScroll = 0
+
+function scrollChange() {
+    let scroll = window.scrollY - oldScroll;
+    oldScroll = window.scrollY;
+    return scroll;
+}
+
+/**
+ * Header Animation.
+ * It gradually hides the header when the user starts to scroll down.
+ * It gradually shows the header when the user starts to scroll up.
+ */
+let translation = 0;
+let translationChange = 0
+
+function headerAnimation(rate, selector) {
     document.addEventListener('scroll', function() {
         const element = document.querySelector(selector);
-        // Update the transform property based on the scroll position
-        element.style.transform = `translateY( ${rate * func() }px)`;
+        const elementHeight = element.clientHeight;
+
+        // it can be a value between 0 and the header heigth
+        if (translationChange >= 0 && translationChange <= elementHeight) {
+            let proposedChange = translationChange + rate * scrollChange(); // calculates t
+            if (proposedChange < 0) proposedChange = 0;
+            if (proposedChange > elementHeight) proposedChange = elementHeight;
+            translationChange = proposedChange;
+        }
+
+        translation = window.scrollY - translationChange;
+        element.style.transform = `translateY( ${translation}px)`;
+
     });
 }
 
+/**
+ * Footer animation.
+ * It displays footer with a parallax effect.
+ */
 
-let translation = 0;
-
-function parallaxAnimation2(rate, selector, func) {
+function footerAnimation(rate, selector, func) {
     document.addEventListener('scroll', function() {
         const element = document.querySelector(selector);
-
-        const offset = (1 - rate) * func();
-        const elementHeight = element.clientHeight;
-        if (offset >= elementHeight) {
-            newValue = func();
-            console.log("Translation", translation)
-            console.log("Func value", newValue)
-            if (translation >= newValue) { // scrolling up
-                translation = newValue;
-            } else {
-                translation = newValue - elementHeight;
-            }
-
-
-        } else {
-            translation = rate * func()
-        }
-
-        element.style.transform = `translateY( ${translation}px)`;
-
         // Update the transform property based on the scroll position
-
+        element.style.transform = `translateY( ${rate * func()}px)`;
     });
 }
