a319d83c68d159640080affae41ea8e1668f7bd1
Yuschoolproject <Fiiona@Yus-MacBook-Pro.local>
2023-11-25 17:32:07 +1300

    Mobile first navigation

---
+++ js/navigation.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const burger = document.getElementById("nav-icon")
+    burger.onclick = function() {
+        console.log("click")
+        const navSection = document.getElementById("menu")
+        navSection.classList.contains("nav-open") ?
+            closeMenu(burger, navSection) :
+            openMenu(burger, navSection)
+    }
+});
+
+function openMenu(burger, menu) {
+    menu.classList.add("nav-open")
+    menu.classList.remove("nav-closed")
+    burger.classList.add("fa-xmark")
+    burger.classList.remove("fa-bars")
+    document.body.classList.add("no-scroll");
+}
+
+function closeMenu(burger, menu) {
+    menu.classList.add("nav-closed")
+    menu.classList.remove("nav-open")
+    burger.classList.remove("fa-xmark")
+    burger.classList.add("fa-bars")
+    document.body.classList.remove("no-scroll");
+}
